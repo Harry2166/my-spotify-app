67493b377ff166b1f5796d9924d78273aa26e018
Harry2166 <harry2166.hq@gmail.com>
2023-07-06 19:31:07 +0800

    Switch to OOP

--- main.py
+++ main.py
@@ -9,77 +9,67 @@ from requests.auth import HTTPBasicAuth
 REDIRECT_URL = "https://www.google.com" # -> this worked? LOL
 BASE_AUTH_URL = "https://accounts.spotify.com/authorize"
 TOKEN_URL = "https://accounts.spotify.com/api/token"
+SCOPE = [
+        "user-read-email",
+        "playlist-read-collaborative"
+        ]
 
-def get_token():
-    username = f"{CLIENT_ID}:{CLIENT_SECRET}"
-    auth_bytes = username.encode("utf-8")
-    encoded = str(base64.b64encode(auth_bytes),'utf8')
+class Spotify:
+    def __init__(self):
+        self.spotify = OAuth2Session(CLIENT_ID, scope=SCOPE, redirect_uri=REDIRECT_URL)
+        self.token = self.get_token()
+        self.headers = {"Authorization":f"Bearer {self.get_token()}"}
+    
+    def authorize(self):
+        authorization_url, state = self.spotify.authorization_url(BASE_AUTH_URL)
+        print('Please go here and authorize: ', authorization_url)
 
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization" : f"Basic {encoded}",
-        "Content-Type" : "application/x-www-form-urlencoded"
-    }
-    data = {"grant_type" : "client_credentials"}
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
+        redirect_response = input('Paste the full redirect URL here: ')
+        auth = HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
 
-universal_token = get_token()
+        token = self.spotify.fetch_token(TOKEN_URL, auth=auth,
+                authorization_response=redirect_response)
+    
+    def get_token(self):
+        username = f"{CLIENT_ID}:{CLIENT_SECRET}"
+        auth_bytes = username.encode("utf-8")
+        encoded = str(base64.b64encode(auth_bytes),'utf8')
 
-def get_auth_header():
-    return {"Authorization":f"Bearer {universal_token}"}
+        url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Authorization" : f"Basic {encoded}",
+            "Content-Type" : "application/x-www-form-urlencoded"
+        }
+        data = {"grant_type" : "client_credentials"}
+        result = post(url, headers=headers, data=data)
+        json_result = json.loads(result.content)
+        token = json_result["access_token"]
+        return token
 
-def get_spotify_id(id,limit=1):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header()
-    query = f"?q={id}&type=artist&limit={limit}"
+    def get_spotify_id(self,id,limit=1):
+        url = "https://api.spotify.com/v1/search"
+        query = f"?q={id}&type=artist&limit={limit}"
 
-    query_url = url + query
-    result = get(query_url, headers=headers)
-    results = json.loads(result.content)  
-    return results["artists"]["items"][0]["id"]
+        query_url = url + query
+        result = self.spotify.get(query_url, headers=self.headers)
+        results = json.loads(result.content)  
+        return results["artists"]["items"][0]["id"]
+    
+    def get_artist(self, artist):
+        artist_id = self.get_spotify_id(artist)
+        url = f"https://api.spotify.com/v1/artists/{artist_id}"
 
-def get_artist(artist):
-    headers = get_auth_header()
-    artist_id = get_spotify_id(artist)
-    url = f"https://api.spotify.com/v1/artists/{artist_id}"
+        result = get(url, headers=self.headers)
+        results = json.loads(result.content)
+        return results
 
-    result = get(url, headers=headers)
-    results = json.loads(result.content)
-    return results
+    def get_my_data(self):
+        result = self.spotify.get("https://api.spotify.com/v1/me", headers=self.headers)
+        results = json.loads(result.content)
 
-def get_my_data():
-    headers = get_auth_header()
-    result = get("https://api.spotify.com/v1/me", headers=headers)
-    results = json.loads(result.content)
-
-    return results
-
-### Spotify OAuth 2
-
-scope = [
-    "user-read-email",
-    "playlist-read-collaborative"
-]
-
-spotify = OAuth2Session(CLIENT_ID, scope=scope, redirect_uri=REDIRECT_URL)
-
-authorization_url, state = spotify.authorization_url(BASE_AUTH_URL)
-print('Please go here and authorize: ', authorization_url)
-
-# Get the authorization verifier code from the callback url
-redirect_response = input('\n\nPaste the full redirect URL here: ')
-
-auth = HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-
-# Fetch the access token
-token = spotify.fetch_token(TOKEN_URL, auth=auth,
-        authorization_response=redirect_response)
-
-print(token)
-
-# Fetch a protected resource, i.e. user profile
-r = spotify.get('https://api.spotify.com/v1/me')
-print(r.content)
+        return results
+    
+spotify = Spotify()
+spotify.authorize()
+print(spotify.get_my_data())
+print(spotify.get_artist("Michael Jackson"))
