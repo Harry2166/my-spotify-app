6b1341562ed75cd67004d32839263455789b0e02
Harry2166 <harry2166.hq@gmail.com>
2023-07-07 16:48:15 +0800

    Implement GUI using Kivy

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import json
 from requests_oauthlib import OAuth2Session
 from requests.auth import HTTPBasicAuth
 from webbrowser import open
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image, AsyncImage
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 REDIRECT_URL = "https://www.google.com" # -> this worked? LOL
 BASE_AUTH_URL = "https://accounts.spotify.com/authorize"
@@ -25,11 +31,13 @@ class Spotify:
         authorization_url, state = self.spotify.authorization_url(BASE_AUTH_URL)
         open(authorization_url)
 
-        redirect_response = input('Paste the full redirect URL here: ')
+    def fetching_token(self, redirect_response):
         auth = HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
 
         token = self.spotify.fetch_token(TOKEN_URL, auth=auth,
                 authorization_response=redirect_response)
+        
+        return token
     
     def get_token(self):
         username = f"{CLIENT_ID}:{CLIENT_SECRET}"
@@ -71,6 +79,91 @@ class Spotify:
         return results
     
 spotify = Spotify()
-spotify.authorize()
-print(spotify.get_my_data())
-print(spotify.get_artist("Michael Jackson"))
+
+class SpotifyApp(App):
+    authorization_data = ""
+    profile_data = ""
+    controls = []
+    def build(self):
+        self.window = GridLayout()
+        self.window.cols = 1
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {"center_x": 0.5, "center_y":0.5}
+
+        self.msg = Label(
+            text="Please login to your Spotify and paste the entire URL of the site after your login"
+        )
+        
+        self.authorize_button = Button(
+            text="Authorize",
+            size_hint= (1,0.5),
+            bold= True,
+            background_color ='#47B5FF',
+            )
+        self.authorize_button.bind(on_press=self.authorize_callback)
+
+        self.redirect_url = TextInput(
+            multiline=False,
+            padding_y= (20,20),
+            size_hint= (1, 0.5)
+            )
+
+        self.submit_button = Button(
+            text="Submit",
+            size_hint= (1,0.5),
+            bold= True,
+            background_color ='#47B5FF',
+            )
+        self.submit_button.bind(on_press=self.submit_callback)
+
+        self.controls = [
+            self.msg,
+            self.authorize_button,
+            self.redirect_url,
+            self.submit_button
+        ]
+
+        self.add_widgets()
+
+        return self.window
+    
+    def add_widgets(self):
+        for elem in self.controls:
+            self.window.add_widget(elem)
+
+    def remove_widgets(self):
+        for elem in self.controls:
+            self.window.remove_widget(elem)
+
+    def authorize_callback(self, event):
+        spotify.authorize()
+
+    def submit_callback(self, event):
+        if self.redirect_url.text == "":
+            print("Empty")
+        else:
+            self.authorization_data = spotify.fetching_token(self.redirect_url.text)
+            self.profile_data = spotify.get_my_data()
+            self.remove_widgets()
+            self.controls = []
+            self.main_page()
+
+    def main_page(self):
+        name = self.profile_data["display_name"]
+        pfp_image_url = self.profile_data["images"][0]["url"]
+
+        self.profile_picture = AsyncImage(
+            source=pfp_image_url,
+            )
+        self.greeting = Label(
+            text=f"Hello {name}!"
+        )
+
+        self.controls = [
+            self.profile_picture,
+            self.greeting
+        ]
+        self.add_widgets()
+
+if __name__ == "__main__":
+    SpotifyApp().run()
